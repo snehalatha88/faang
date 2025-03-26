6812781d846530bc788a36fcdc097e26d19a5102
Sneha <snehakaturi12@gmail.com>
2025-03-26 17:35:27 +0530

    hiii co

--- src/main/java/com/org/project1/test.java
+++ src/main/java/com/org/project1/test.java
@@ -2,6 +2,6 @@ package com.org.project1;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hi this is sneha");
+        System.out.println(" this is sneha");
     }
 }
